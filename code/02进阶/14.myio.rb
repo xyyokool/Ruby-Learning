7221370b83b08d501206bec5345b7556151d7c17
stan <staninjapan2015@gmail.com>
2022-08-16 21:59:10 +0800

    update read file note

--- code/02进阶/14.myio.rb
+++ code/02进阶/14.myio.rb
@@ -1,2 +1,31 @@
-a = gets
-puts a
+# a = gets
+# puts a
+
+# lines = readlines
+# puts lines.length
+#
+# Dir::pwd
+
+# File.open('text.txt') do |f|
+#   puts f.gets
+# end
+
+# f = File.new("text.txt", "r")
+# puts f.gets
+# f.close
+
+class MyFile
+  attr_reader :handle
+
+  def initialize(filename)
+    @handle = File.new(filename, "r")
+  end
+
+  def finished
+    @handle.close
+  end
+end
+
+f = MyFile.new("text.txt")
+puts f.handle.gets
+f.finished
