4b1439c5388488d6c8ebc4efce17022463c50503
stan <staninjapan2015@gmail.com>
2022-08-25 23:20:34 +0800

    update fork

--- code/03高级/02.与其他系统交互.rb
+++ code/03高级/02.与其他系统交互.rb
@@ -3,5 +3,22 @@ x = system('echo hello ruby')
 y = `echo hello ruby too`
 z = %x{echo hello ruby too 2}
 
-exec "echo this is exec way"
-puts "never be here"
+# exec "echo this is exec way"
+# puts "never be here"
+
+
+# if fork.nil?
+#   exec "echo fork here"
+# end
+#
+# puts "test fork"
+
+child = fork do
+  sleep 3
+  puts "Childs says hi!"
+end
+
+puts "waiting childs"
+
+Process.wait child
+puts "All Done!"
