fb67e0e8d8440185a40385aa8d17cd25b358362d
stan <staninjapan2015@gmail.com>
2022-08-27 23:17:21 +0800

    update fibers note

--- code/03高级/03.多线程.rb
+++ code/03高级/03.多线程.rb
@@ -23,3 +23,28 @@ sg = Fiber.new do
 end
 
 10.times {puts sg.resume}
+
+# fiber
+
+sg = Fiber.new do
+  s = 0
+  loop do
+    square = s * s
+    s += 1
+    # Fiber.yield会把square的结果返回给s。
+    s = Fiber.yield(square) || s
+  end
+end
+
+puts sg.resume
+puts sg.resume
+puts sg.resume
+puts sg.resume 50
+
+# non_blocking_fiber
+non_blocking = Fiber.new(blocking: false) do
+  puts "Blocking Fiber ? #{Fiber.current.blocking?}"
+  sleep 2
+end
+
+3.times { puts non_blocking.resume }
